fca5a34dc71be55db2da110f4f5e125586db1721
jknight9228 <jknight@Jamess-MacBook-Pro-5.local>
2013-09-15 15:21:48 -1000

    Support for red input boxes on error
    Added in setAttribute to set the color of the input box to red if an
    error occurred and also added an else statement to reset the color when
    a user enters input in the field.

--- A10/js/validateForm.js
+++ A10/js/validateForm.js
@@ -12,21 +12,48 @@ function validateForm()
 	if (name == "") 
 	{
 		document.getElementById("nameError").style.display = "inline";
+		document.getElementById("name").setAttribute("style", "background-color: red");
 		error = 1;
 	}
+	else  
+	{
+	    document.getElementById("name").setAttribute("style", "background-color: null");
+	}
+	
+	
 	if (email == "") 
 	{
 		document.getElementById("emailError").style.display = "inline";
+		document.getElementById("email").setAttribute("style", "background-color: red");
 		error = 1;
 	}
+		else  // Make sure color returns to normal, or stays normal
+	{
+	    document.getElementById("email").setAttribute("style", "background-color: null");
+	}
+	
+	
 	if (password == "") 
 	{
 		document.getElementById("passwordError").style.display = "inline";
+		document.getElementById("password").setAttribute("style", "background-color: red");
 		error = 1;
 	}
+		else  // Make sure color returns to normal, or stays normal
+	{
+	    document.getElementById("password").setAttribute("style", "background-color: null");
+	}
+	
+	
 	if (password != confirm)
 	{
 		document.getElementById("confirmError").style.display = "inline";
+		document.getElementById("confirm").setAttribute("style", "background-color: red");
+		error = 1;
+	}
+	else  // Make sure color returns to normal, or stays normal
+	{
+	    document.getElementById("confirm").setAttribute("style", "background-color: null");
 	}
 	
 	
