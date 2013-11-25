dad2547a5fd9376bd99014ab7596b4ff81aee4db
jknight9228 <jknight@Jamess-MacBook-Pro-5.local>
2013-11-24 23:15:44 -1000

    Using framework for login/registering
    Changed structure so links go to iframe target, that way we don’t need
    to recreate the wheel with each page made

--- final_project/setGT.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-	if(isset($_POST["gt"])){
-		// Set the cookie for immediate access by gt_lookup
-		setcookie("gamertag", $_POST["name"], time()+3600);
-		$_COOKIE["gamertag"] = $_POST["gt"];
-	}
-	echo $_COOKIE["gamertag"];
-?>
--- final_project/views/not_logged_in.php
+++ final_project/views/not_logged_in.php
@@ -1,3 +1,5 @@
+
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -39,19 +41,20 @@
 	<!-- navbar -->
 	<nav class="navbar navbar-default" id="mainnav" role="navigation">
 		<div class="navbar-header">
-			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
 				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
+				<span class="icon-bar"><font color="black">text</font></span>
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
 		</div>
-
-		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse" id="navbar-collapse">
 			 <ul class="nav navbar-nav">
-				 <li class="active"><a href="index.html">Home</a></li>
-				 <li class="active"><a href="gt_lookup_search.html">XBOX Live Gamertag Lookup </a></li>
-				 <li class="active"><a href="about.html">About</a></li>
+				 <li class="active"><a href="index.html" target="iFrameGT">Home</a></li>
+				 <li class="active"><a href="gt_lookup.php" target="iFrameGT">XBOX Live Gamertag Lookup </a></li>
+				 <li class="active"><a href="about.html" target="iFrameGT">About</a></li>
 			 </ul>
 		</div>
 	 <!-- /#main-nav --> 
@@ -69,10 +72,43 @@
 					 <section class="widget clearfix">
 						 <!-- Login -->
 						 <h4 class="widgettitle">Login</h4>
-						 <form class="form-group" name="login" action="test.php" onsubmit="return validateLogin()" method="post">
-							 Username: <input type="text" class="form-control" name="uname" placeholder="Name"></br>
-							 Password: <input type="password" class="form-control" name="pword" placeholder="Password">
-							 <input type="submit" value="Submit">
+
+							 
+							 <!-- errors & messages --->
+<?php
+
+// show negative messages
+if ($login->errors) {
+    foreach ($login->errors as $error) {
+        echo $error;    
+    }
+}
+
+// show positive messages
+if ($login->messages) {
+    foreach ($login->messages as $message) {
+        echo $message;
+    }
+}
+
+?>
+<!-- errors & messages --->
+
+<!-- login form box -->
+<form method="post" action="index.php" name="loginform">
+
+    <label for="login_input_username">Username</label>
+    <input id="login_input_username" class="login_input" type="text" name="user_name" required />
+
+    <label for="login_input_password">Password</label>
+    <input id="login_input_password" class="login_input" type="password" name="user_password" autocomplete="off" required />
+
+    <input type="submit"  name="login" value="Log in" />
+
+</form>
+
+<div><a href="register.php" target="iFrameGT">Register new account</a></div>
+
 						 </form>
 						 <!-- End login -->
 					 </section>
@@ -81,28 +117,18 @@
 			 </div>
 			 <!-- /#Left sidebar -->
 	
-			 <div class="col-xs-12 col-sm-12 col-md-9">
+			 <div class="col-xs-12 col-sm-12 col-md-6">
 				 <div class="container" style="background: white;" id="cont">
 					 <article class="post clearfix">
 						 <header>
-							<h1 id="content-title">Xbox Live Gamertag Lookup</h1>	
+							 <h1 id="content-title">Main Page</h1>
 						 </header>
-						 <!-- Search -->
-						 <form name="serachgt" role="search" action="setGT.php" onsubmit="" method="post">
-							<div class="input-group input-group-lg">
-								<input type="text" class="form-control" name="gt" placeholder="Search for a Gamertag! Press enter when ready" autofocus="autofocus">
-								<span class="input-group-btn">
-									<input type="submit" class="btn btn-default" value="Search">
-								</span>
-							</div>
-						</form>
-						 
-						 
-						 <iframe name="iFrameGT" id="gtFrame" width="800" height="1200" src="gt_lookup.php" 
+							<p>
+						 <iframe name="iFrameGT" id="gtFrame" width="100%" height="100%" src="landing.html" 
 								 marginwidth="1" marginheight="1" frameborder="0" vspace="1" hspace="1">
 								<p>If you see this, your browser doesn't support frames.</p>
 						 </iframe>
-						 
+							</p>
 					 </article>
 				 <!-- /.post -->
 				 </div>
